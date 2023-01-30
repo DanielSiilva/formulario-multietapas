fe9efbed4f107d4c44ac8f19ebce734f0d1b65c6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-01-29 22:43:21 -0300

    style: add estilização inicial a pagina form1

--- src/pages/Form1/index.tsx
+++ src/pages/Form1/index.tsx
@@ -7,9 +7,33 @@ export function Form1(){
     return(
 
         <C.Container>
-            <p>Bem-vindo(a)</p>
+            <C.TitleAndSubtitle>
+                <p>Bem-vindo(a)</p>
 
-       
+                <h1>Preencha os campos abaixo</h1>
+            </C.TitleAndSubtitle>
+           
+
+            <C.FormContainer>
+                <div>
+                    <label>Nome</label>
+                    <input  
+                        placeholder='Seu nome'
+                        required
+                    
+                    />
+                </div>
+                
+                <div>
+                    <label>Stack</label>
+                    <input  
+                        placeholder='ex: ReactJs'
+                        required
+                    />
+                </div>
+                
+                <button type='submit'>Proxima</button>
+            </C.FormContainer>
         </C.Container>
     )
 }
--- src/pages/Form1/styled.ts
+++ src/pages/Form1/styled.ts
@@ -1,15 +1,77 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    border: 1px solid black;
 
     display: flex;
-    
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
 
     width: 50vw;
     height: 80vh;
 
+
     padding: 1rem;
+
+`
+
+
+export const TitleAndSubtitle = styled.div`
+     display: flex;
+     flex-direction: column;
+     gap: 0.5rem;
+
+     h1{
+        font-size: 1.5rem;
+        font-weight: bold;
+     }
+
+`
+
+export const FormContainer = styled.form`
+    display: flex;
+    flex-direction: column;
+    
+    gap: 0.5rem;
+
+    margin-top: 3rem;
+
+    
+    button{
+            width: 20rem;
+            height: 2rem;
+            padding: 0.5rem;
+
+            color: ${props => props.theme['white']} ;
+            background: ${props => props.theme['green-light']};
+
+            border: none;
+            border-radius: 4px;
+
+            cursor: pointer;
+    }
+
+
+    div{
+        display: flex;
+        flex-direction: column;
+        gap: 0.3rem;
+
+        label{
+            font-size: 1rem;
+            font-weight: bold;
+
+            color: ${props => props.theme['gray']};
+        }
+
+        input{
+            width: 20rem;
+            height: 2rem;
+            padding: 0.5rem;
+        }
+
+    }
 
 
 `
